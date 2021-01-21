4f96ab496f9f5b66f6f612406eacc34b5fbc445e
miyamatakuya <1029takuya@gmail.com>
2021-01-22 01:23:02 +0900

    commit30

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, only: [:edit]
+  before_action :move_to_index, only: [:edit,:destroy]
   before_action :authenticate_user! ,only:[:new,:edit, :update]
-  before_action :set_item,only:[:edit,:show,:update]
+  before_action :set_item,only:[:edit,:show,:update,:destroy]
 
   
   def index
@@ -9,7 +9,7 @@ class ItemsController < ApplicationController
   end
  
 def edit
-    
+  
 end
 
 
@@ -27,7 +27,13 @@ end
   end
 
   def show
-    
+  end
+
+
+  def destroy
+    if @item.destroy
+      redirect_to root_path
+      end
   end
 
 

f7d1b02731a1466aa6a797b1a59c4ce65d8bdd85
miyamatakuya <1029takuya@gmail.com>
2021-01-21 16:11:28 +0900

    commit29

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -47,7 +47,7 @@ end
 
   def move_to_index
     @item= Item.find(params[:id])
-    unless current_user.id == @item.user_id
+    unless user_signed_in? && current_user.id == @item.user_id
       redirect_to action: :index
     end
   end

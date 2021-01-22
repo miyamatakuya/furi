36f78a05d0a457c766afc147db089f66398d2c12
miyamatakuya <1029takuya@gmail.com>
2021-01-22 15:42:57 +0900

    commit32

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, only: [:edit,:destroy]
   before_action :authenticate_user! ,only:[:new,:edit, :update]
   before_action :set_item,only:[:edit,:show,:update,:destroy]
+  before_action :move_to_index, only: [:edit,:destroy]
+ 
 
   
   def index
@@ -52,8 +53,7 @@ end
   end
 
   def move_to_index
-    @item= Item.find(params[:id])
-    unless user_signed_in? && current_user.id == @item.user_id
+    unless  current_user.id == @item.user_id
       redirect_to action: :index
     end
   end
